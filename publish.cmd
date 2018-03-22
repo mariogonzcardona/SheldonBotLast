83af47a40484588c15120da4b84a71a564236d91
Mario Alejandro Gonzalez Cardona <alejandrogonzalez@uadec.edu.mx>
2018-03-21 18:31:25 -0600

    Agregar archivos de proyecto.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=sheldonbotlast-Web-Deploy.pubxml -p:Password=Gmp8X7Yios7AiievTayAnspuWbj82xhvM38AYavxoaMqA8588nTRnRRAeJfj
+
